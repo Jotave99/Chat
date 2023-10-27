4db8cba8d45a42de8bd74b52dd83f528bc08d28a
João Victor <joaobva1016@gmail.com>
2023-10-26 21:28:47 -0300

    Add nick and date

--- cliente.py
+++ cliente.py
@@ -1,14 +1,20 @@
 import socket;
+import datetime;
 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 
 cliente.connect(('localhost', 8888));
 
+data = datetime.date.today();
+dataFormatada = data.strftime("%d/%m/%y");
+
+nick = input('Digite o seu nick: ');
+
 terminado = False;
 print('Digite tt para terminar o chat');
 
 while not terminado:
-    cliente.send(input('Mensagem: ').encode('utf-8'))
+    cliente.send(input('['+dataFormatada+'] ['+nick +']: ').encode('utf-8'))
     msg = cliente.recv(1024).decode('utf-8')
     if msg == 'tt':
         terminado = True
